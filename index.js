12a9d18b901e26070f1f66d4458d6599a81f3029
Rahultadmybrand <rt040371@gmail.com>
2023-12-30 15:53:22 +0530

    changed pino logger name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const pino = require("pino");
 
-const logger = (options = {}, destination = "./pino-logger.log") =>
+const pinoLogger = (options = {}, destination = "./pino-logger.log") =>
   pino(options, pino.destination(destination));
 
-module.exports = logger;
+module.exports = pinoLogger;
